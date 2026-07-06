0bcdc4ca412b73dbd8dca159fde849e55418232b
Your Name <you@example.com>
2026-07-06 14:32:47 +0700

    fix: implement is_half_holiday locally instead of importing from erpnext

--- hrms/hr/doctype/shift_type/shift_type.py
+++ hrms/hr/doctype/shift_type/shift_type.py
@@ -21,7 +21,8 @@ from frappe.utils import (
 )
 
 from erpnext.setup.doctype.employee.employee import get_holiday_list_for_employee
-from erpnext.setup.doctype.holiday_list.holiday_list import is_half_holiday, is_holiday
+from erpnext.setup.doctype.holiday_list.holiday_list import is_holiday
+from hrms.utils import is_half_holiday
 
 from hrms.hr.doctype.attendance.attendance import mark_attendance
 from hrms.hr.doctype.employee_checkin.employee_checkin import (
